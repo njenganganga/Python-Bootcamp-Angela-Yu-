ec9b13114240848c0dc606f5a6318bffdfc0d393
njenganganga <112175070+njenganganga@users.noreply.github.com>
2025-09-10 15:47:03 +0300

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,17 @@
+from traceback import print_tb
+
+len("Hello")
+print("Hello"[4])
+print("Hello"[-1])
+
+# integer = whole number
+print(123+456)
+#For cleaner code, you can separate integers with an underscore.
+print(123_456_789)
+
+#float = floating numbers/decimal numbers
+print(3.142)
+
+#boolean=true or false
+print(True)
+print(False)
---
+++ typeerror.py
@@ -0,0 +1,11 @@
+len("12345")
+print(len("12345"))
+print(type("Hello"))
+#type is the function to tell you what is the data type in the brackets.
+
+print(type("Hello"))
+print(type(1234))
+print(type(3.142))
+print(type(False))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
